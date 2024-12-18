8b396c76501cb66983db4bd242e221c367e5d38b
Syed Yead Zaman <s.yead.zaman@gmail.com>
2024-12-18 14:31:29 -0500

    terminal usage

---
+++ terminal.asm
@@ -0,0 +1,16 @@
+#include terminal_lib.asm
+
+main:
+call t_print_h
+call t_print_e
+call t_print_l
+call t_print_l
+call t_print_o
+call t_print_space
+call t_print_w
+call t_print_o
+call t_print_r
+call t_print_l
+call t_print_d
+call t_print_exclamation
+halt
