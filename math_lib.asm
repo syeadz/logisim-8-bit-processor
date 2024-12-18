4e5e83e52caa97dfd253c6bca3f97237a437a38e
Syed Yead Zaman <s.yead.zaman@gmail.com>
2024-12-18 18:47:39 -0500

    division working

--- math_lib.asm
+++ math_lib.asm
@@ -5,7 +5,9 @@
 ; Description: Multiply two non-negative 8-bit numbers
 ; Input: $5 = multiplicand, $6 = multiplier
 ; Output: $7 = result
+; Side effects: $6 is modified
 m_mul8:
+clr $7
 cmp $0, $5
 jpZ m_ret_0
 cmp $0, $6
@@ -17,6 +19,29 @@ jpZ m_ret
 add $7, $5
 dec $6
 goto m_mul8_loop
+
+
+; Function: div8
+; Description: Divide two non-negative 8-bit numbers
+; Input: $5 = dividend, $6 = divisor
+; Output: $7 = result, $5 = remainder
+; Note: does not handle divide by 0 errors, returns as 0
+m_div8:
+clr $7
+cmp $0, $5
+jpZ m_ret_0
+cmp $0, $6
+jpZ m_ret_0
+m_div8_loop:
+; dividend < divisor, so return
+cmp $5, $6
+jpC m_ret
+sub $5, $6
+inc $7
+; division complete, return
+cmp $5, $0
+jpZ m_ret
+goto m_div8_loop
 
 
 ; Set return value to 0 and return
