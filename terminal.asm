353090923ee36e8a5ad565467f3d60ce755c37c6
Syed Yead Zaman <s.yead.zaman@gmail.com>
2024-12-19 14:34:43 -0500

    close #13

--- terminal.asm
+++ terminal.asm
@@ -1,4 +1,14 @@
+; Showcasing the terminal library
+; This program prints "Hello world!" to the terminal
+; It also demonstrates the use of the math library to multiply and divide two numbers
+; which is then printed to the terminal
+
 #include terminal_lib.asm
+
+#define MUL_A 0x27
+#define MUL_B 0x6
+#define DIV_A 0xFF
+#define DIV_B 0x2F
 
 main:
 call t_print_h
@@ -13,4 +23,48 @@ call t_print_r
 call t_print_l
 call t_print_d
 call t_print_exclamation
+
+call t_print_newline
+
+lwi $7, MUL_A
+mov $5, $7
+call t_print_reg_num
+call t_print_space
+call t_print_multiply
+call t_print_space
+lwi $7, MUL_B
+mov $6, $7
+call t_print_reg_num
+call t_print_space
+call t_print_equal
+call t_print_space
+call m_mul8
+call t_print_reg_num
+
+call t_print_newline
+
+lwi $7, DIV_A
+mov $5, $7
+call t_print_reg_num
+call t_print_space
+call t_print_divide
+call t_print_space
+lwi $7, DIV_B
+mov $6, $7
+call t_print_reg_num
+call t_print_space
+call t_print_equal
+call t_print_space
+call m_div8
+call t_print_reg_num
+call t_print_comma
+call t_print_space
+call t_print_r
+call t_print_e
+call t_print_m
+call t_print_colon
+call t_print_space
+mov $7, $5
+call t_print_reg_num
+
 halt
--- terminal_lib.asm
+++ terminal_lib.asm
@@ -2,250 +2,312 @@
 ; Only capable of printing single ascii characters
 ; Unable to read data from ROM, so the ascii characters are hardcoded
 
+#include math_lib.asm
+
 #define T_TERM_ADDR 0x7F
 
+; Todo: replace $7 with another register if it causes problems
 t_print_a:
-lwi $4, 0x41
+lwi $7, 0x41
 goto t_print
 
 t_print_b:
-lwi $4, 0x42
+lwi $7, 0x42
 goto t_print
 
 t_print_c:
-lwi $4, 0x43
+lwi $7, 0x43
 goto t_print
 
 t_print_d:
-lwi $4, 0x44
+lwi $7, 0x44
 goto t_print
 
 t_print_e:
-lwi $4, 0x45
+lwi $7, 0x45
 goto t_print
 
 t_print_f:
-lwi $4, 0x46
+lwi $7, 0x46
 goto t_print
 
 t_print_g:
-lwi $4, 0x47
+lwi $7, 0x47
 goto t_print
 
 t_print_h:
-lwi $4, 0x48
+lwi $7, 0x48
 goto t_print
 
 t_print_i:
-lwi $4, 0x49
+lwi $7, 0x49
 goto t_print
 
 t_print_j:
-lwi $4, 0x4A
+lwi $7, 0x4A
 goto t_print
 
 t_print_k:
-lwi $4, 0x4B
+lwi $7, 0x4B
 goto t_print
 
 t_print_l:
-lwi $4, 0x4C
+lwi $7, 0x4C
 goto t_print
 
 t_print_m:
-lwi $4, 0x4D
+lwi $7, 0x4D
 goto t_print
 
 t_print_n:
-lwi $4, 0x4E
+lwi $7, 0x4E
 goto t_print
 
 t_print_o:
-lwi $4, 0x4F
+lwi $7, 0x4F
 goto t_print
 
 t_print_p:
-lwi $4, 0x50
+lwi $7, 0x50
 goto t_print
 
 t_print_q:
-lwi $4, 0x51
+lwi $7, 0x51
 goto t_print
 
 t_print_r:
-lwi $4, 0x52
+lwi $7, 0x52
 goto t_print
 
 t_print_s:
-lwi $4, 0x53
+lwi $7, 0x53
 goto t_print
 
 t_print_t:
-lwi $4, 0x54
+lwi $7, 0x54
 goto t_print
 
 t_print_u:
-lwi $4, 0x55
+lwi $7, 0x55
 goto t_print
 
 t_print_v:
-lwi $4, 0x56
+lwi $7, 0x56
 goto t_print
 
 t_print_w:
-lwi $4, 0x57
+lwi $7, 0x57
 goto t_print
 
 t_print_x:
-lwi $4, 0x58
+lwi $7, 0x58
 goto t_print
 
 t_print_y:
-lwi $4, 0x59
+lwi $7, 0x59
 goto t_print
 
 t_print_z:
-lwi $4, 0x5A
+lwi $7, 0x5A
 goto t_print
 
 t_print_space:
-lwi $4, 0x20
+lwi $7, 0x20
 goto t_print
 
 t_print_exclamation:
-lwi $4, 0x21
+lwi $7, 0x21
 goto t_print
 
 t_print_double_quote:
-lwi $4, 0x22
+lwi $7, 0x22
 goto t_print
 
 t_print_hash:
-lwi $4, 0x23
+lwi $7, 0x23
 goto t_print
 
 t_print_dollar:
-lwi $4, 0x24
+lwi $7, 0x24
 goto t_print
 
 t_print_percent:
-lwi $4, 0x25
+lwi $7, 0x25
 goto t_print
 
 t_print_ampersand:
-lwi $4, 0x26
+lwi $7, 0x26
 goto t_print
 
 t_print_single_quote:
-lwi $4, 0x27
+lwi $7, 0x27
 goto t_print
 
 t_print_open_paren:
-lwi $4, 0x28
+lwi $7, 0x28
 goto t_print
 
 t_print_close_paren:
-lwi $4, 0x29
+lwi $7, 0x29
 goto t_print
 
 t_print_asterisk:
-lwi $4, 0x2A
+lwi $7, 0x2A
 goto t_print
 
 t_print_plus:
-lwi $4, 0x2B
+lwi $7, 0x2B
 goto t_print
 
 t_print_comma:
-lwi $4, 0x2C
+lwi $7, 0x2C
 goto t_print
 
 t_print_minus:
-lwi $4, 0x2D
+lwi $7, 0x2D
 goto t_print
 
 t_print_dot:
-lwi $4, 0x2E
+lwi $7, 0x2E
 goto t_print
 
 t_print_slash:
-lwi $4, 0x2F
+lwi $7, 0x2F
 goto t_print
 
 t_print_colon:
-lwi $4, 0x3A
+lwi $7, 0x3A
 goto t_print
 
 t_print_semicolon:
-lwi $4, 0x3B
+lwi $7, 0x3B
 goto t_print
 
 t_print_less_than:
-lwi $4, 0x3C
+lwi $7, 0x3C
 goto t_print
 
 t_print_equal:
-lwi $4, 0x3D
+lwi $7, 0x3D
 goto t_print
 
 t_print_greater_than:
-lwi $4, 0x3E
+lwi $7, 0x3E
 goto t_print
 
 t_print_question:
-lwi $4, 0x3F
+lwi $7, 0x3F
 goto t_print
 
 t_print_at:
-lwi $4, 0x40
+lwi $7, 0x40
 goto t_print
 
 t_print_newline:
-lwi $4, 0x0A
+lwi $7, 0x0A
 goto t_print
 
 t_print_0:
-lwi $4, 0x30
+lwi $7, 0x30
 goto t_print
 
 t_print_1:
-lwi $4, 0x31
+lwi $7, 0x31
 goto t_print
 
 t_print_2:
-lwi $4, 0x32
+lwi $7, 0x32
 goto t_print
 
 t_print_3:
-lwi $4, 0x33
+lwi $7, 0x33
 goto t_print
 
 t_print_4:
-lwi $4, 0x34
+lwi $7, 0x34
 goto t_print
 
 t_print_5:
-lwi $4, 0x35
+lwi $7, 0x35
 goto t_print
 
 t_print_6:
-lwi $4, 0x36
+lwi $7, 0x36
 goto t_print
 
 t_print_7:
-lwi $4, 0x37
+lwi $7, 0x37
 goto t_print
 
 t_print_8:
-lwi $4, 0x38
+lwi $7, 0x38
 goto t_print
 
 t_print_9:
-lwi $4, 0x39
+lwi $7, 0x39
 goto t_print
 
-; Print an ascii character stored in register $5 and return
+t_print_plus:
+lwi $7, 0x2B
+goto t_print
+
+t_print_minus:
+lwi $7, 0x2D
+goto t_print
+
+t_print_divide:
+lwi $7, 0x2F
+goto t_print
+
+t_print_multiply:
+lwi $7, 0x2A
+goto t_print
+
+; Print an ascii character stored in register $7 and return
 t_print:
 lwi $2, T_TERM_ADDR
-sw $2, $5
+sw $2, $7
+ret
+
+; Clear the terminal
+t_clear:
+lwi $2, T_TERM_ADDR
+lwi $7, 0x80
+sw $2, $7
+ret
+
+
+; Function: print_reg_num
+; Description: Prints the number stored in register $7 to the terminal
+; Input: $7 = number to print
+; Note: Only works for 8-bit numbers (0-255)
+t_print_reg_num:
+push $5
+push $6
+push $7
+lwi $10, 0 ; digit counter
+lwi $6, 10 ; divide by 10
+mov $5, $7 ; save number in $5 as dividend
+t_print_reg_num_div_loop:
+cmp $7, $0 ; check if quotient/number is 0
+jpZ t_print_reg_num_div_loop_end
+call m_div8
+push $5 ; save remainder (digit)
+mov $5, $7 ; save quotient as dividend
+inc $10 ; increment digit counter
+goto t_print_reg_num_div_loop
+t_print_reg_num_div_loop_end:
+cmp $10, $0 ; check if digit counter is 0
+jpZ t_print_0 ; print 0 if number is 0
+t_print_reg_num_print_loop:
+pop $7 ; get digit
+addi $7, 0x30 ; convert to ascii
+lwi $2, T_TERM_ADDR
+sw $2, $7 ; print digit
+dec $10 ; decrement digit counter
+cmp $10, $0 ; check if all digits have been printed
+jpNZ t_print_reg_num_print_loop
+pop $7
+pop $6
+pop $5
 ret
