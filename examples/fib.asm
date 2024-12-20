ea76433c9daa68d0cdaee29fe974aee03e9edb44
Syed Yead Zaman <s.yead.zaman@gmail.com>
2024-12-19 19:07:16 -0500

    rearrange fib

--- examples/fib.asm
+++ examples/fib.asm
@@ -1,8 +1,7 @@
-# Calculates the fibonacci sequence up to 13 (233)
+; Calculates the fibonacci sequence up to 13 (233)
+; And displays the result in BCD
 
-#define bcd0 0x1F
-#define bcd1 0x3F
-#define bcd2 0x5F
+#include lib/bcd_lib.asm
 
 main:
     lwi $7, 0 # fib 0
@@ -10,11 +9,6 @@ main:
 
     lwi $9, 0 # counter
     lwi $10, 12 # max
-
-    lwi $13, bcd0 # address for bcd0
-    lwi $14, bcd1 # address for bcd1
-    lwi $15, bcd2 # address for bcd2
-
 
 fib:
     # display numbers
@@ -34,5 +28,5 @@ fib:
 
 end:
     # print fib to bcd
-    sw $15, $7 # store fib result
+    call b_bcd0_display
     halt
