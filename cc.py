1cec3bfe596233b4d4084f670dab9db49589e88c
Syed Yead Zaman <s.yead.zaman@gmail.com>
2024-12-21 19:47:42 -0500

    basic tokenizer

---
+++ cc.py
@@ -0,0 +1,28 @@
+import re
+
+# Define token types
+# https://learn.microsoft.com/en-us/cpp/c-language/lexical-grammar?view=msvc-170
+TOKENS = [
+    ("KEYWORD", r"\b(break|continue|char|if|else|for|return|void)\b"),
+    ("IDENTIFIER", r"\b[a-zA-Z_][a-zA-Z0-9_]*\b"),
+    ("CONSTANT", r"\b0[bB][01]+|0[oO][0-7]+|0[xX][0-9a-fA-F]+|\d+\b"),
+    ("PUNCTUATOR", r"[;{}()+\-*/=]"),
+    ("WHITESPACE", r"\s+"),  # Ignore whitespace
+]
+
+def tokenize(code):
+    tokens = []
+    while code:
+        for token_type, regex in TOKENS:
+            match = re.match(regex, code)
+            if match:
+                if token_type != "WHITESPACE":  # Skip whitespace
+                    tokens.append((token_type, match.group(0)))
+                code = code[match.end():]
+                break
+        else:
+            raise SyntaxError(f"Unexpected character: {code[0]}")
+    return tokens
+
+code = "char a = 5; return a + 0x10;"
+print(tokenize(code))
